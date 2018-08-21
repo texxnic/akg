49aa3ccb819c52881d2eaf319cf7cb3c0dbdadf1
Roman <texxnic@gmail.com>
2018-08-22 00:17:30 +0300

    2207

---
+++ testom.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Амальгама консалтинг-групп</title>
+    <?php include_once 'modules/head.php';?>
+</head>
+
+<body>
+    <?php include_once 'modules/header.php';?>
+    <div class="pageContent pricesSec">
+        <div class="pageJumbo">
+            <div class="container-lessfluid">
+                <div class="col-md-6 jumboContent">
+                    <h2>Цены</h2>
+                    <h5 class="pageJumbo__description paddedText">Стоимость обслуживания в «Амальгама консалтинг-групп» для каждого клиента рассчитываются индивидуально, и зависит от множества факторов. Примерную стоимость обслуживания вы можете рассчитать ниже, воспользовавшись "Калькулятором примерной стоимости".
+                    </h5>
+                </div>
+            </div>
+        </div>
+        <div class="pageDescription">
+            <div id="root"></div>
+        </div>
+    </div>
+    <div class="pageFooter footer">
+        <?php include_once 'modules/footer.php';?>
+    </div>
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/plugins/CSSPlugin.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/easing/EasePack.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/TweenLite.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/TweenMax.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script> -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.min.js"></script>
+<script src="assets/js/scrolloverflow.min.js"></script>
+<script src="assets/js/fullpage.extensions.min.js"></script>
+<script src="assets/js/fullpage.min.js"></script>
+<script src="assets/js/textAnim.js"></script>
+<script src="assets/js/calc.js"></script>
+<script src="assets/js/odometer.min.js"></script>
+<script src="assets/js/main.aecabbcb.js"></script>
+
+
+</html>
